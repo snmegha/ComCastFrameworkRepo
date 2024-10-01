ecdfd0b7d72f4c4ca65d677984888d9ed6122052
snmegha <meghasn656@gmail.com>
2024-10-01 10:03:19 +0530

    i made some changes

--- PracticeProject/src/test/java/package1/sample1.java
+++ PracticeProject/src/test/java/package1/sample1.java
@@ -5,6 +5,8 @@ public class sample1 {
 	public static void main(String[] args) {
 		System.out.println("im sample 1 from package1");
 		System.out.println("im 2nd sop");
+		System.out.println("im 2nd sop");
+		System.out.println("im 2nd sop");
 
 	}
 
